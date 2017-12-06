9604c5afebb878d318481c60e1e95fc4466a1d53
joelee2017 <as955133@gmail.com>
2017-12-06 16:46:55 +0800

    完成讀條

--- ProgressBar/Form1.Designer.cs
+++ ProgressBar/Form1.Designer.cs
@@ -29,11 +29,91 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtSec = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.LblMsg = new System.Windows.Forms.Label();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "請輸入倒數秒數：";
+            // 
+            // TxtSec
+            // 
+            this.TxtSec.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtSec.Location = new System.Drawing.Point(200, 72);
+            this.TxtSec.Multiline = true;
+            this.TxtSec.Name = "TxtSec";
+            this.TxtSec.Size = new System.Drawing.Size(134, 32);
+            this.TxtSec.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(37, 144);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(340, 42);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // LblMsg
+            // 
+            this.LblMsg.AutoSize = true;
+            this.LblMsg.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblMsg.Location = new System.Drawing.Point(33, 234);
+            this.LblMsg.Name = "LblMsg";
+            this.LblMsg.Size = new System.Drawing.Size(63, 19);
+            this.LblMsg.TabIndex = 3;
+            this.LblMsg.Text = "label2";
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnStart.Location = new System.Drawing.Point(301, 228);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(105, 31);
+            this.BtnStart.TabIndex = 4;
+            this.BtnStart.Text = "開始";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 303);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.LblMsg);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.TxtSec);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtSec;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label LblMsg;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- ProgressBar/Form1.cs
+++ ProgressBar/Form1.cs
@@ -16,5 +16,37 @@ namespace ProgressBar
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            progressBar1.Step = 1;
+            LblMsg.Text = "輸入秒數後按 開始 鈕";
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                progressBar1.Maximum = int.Parse(TxtSec.Text);
+                progressBar1.Value = 0;
+                timer1.Enabled = true;
+            }
+            catch
+
+            { LblMsg.Text = "請輸入正整數 !";return;}//錯誤訊息
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.PerformStep();
+            LblMsg.Text = progressBar1.Value + "秒";
+            if(progressBar1.Value==progressBar1.Maximum)
+            {
+                LblMsg.Text = "倒數時間到 !";
+                timer1.Enabled = false; //計時器停止
+            }
+        }
     }
 }
